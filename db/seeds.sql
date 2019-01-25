7d3a7db7087cc37b11588712052771af145ef6d8
Melvin Hernandez <melher39@gmail.com>
2019-01-25 11:07:06 -0600

    set up mysql database and table with 3 sample entries through these files schema.sql and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Monster Burger", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Old Fashioned", TRUE);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Veggie Burger", FALSE);
