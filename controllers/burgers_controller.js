e7137a557beb5d6c59ec7a5e649f8f5434bf276c
Melvin Hernandez <melher39@gmail.com>
2019-01-25 22:51:35 -0600

    created controllers folder with burgers_controller.js file inside and imported express, burger.js and created the express router

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,8 @@
+// express node package imported
+const express = require("express");
+
+// express router for the app
+const router = express.Router();
+
+// import the burger.js 
+const burger = require("../models/burger.js")
