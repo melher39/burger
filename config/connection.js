2afb243d9969fa1901aa86b4fdf586109c08b02b
Melvin Hernandez <melher39@gmail.com>
2019-01-28 16:01:29 -0600

    added JawsDB connection to connection.js and added get route to burgers_controller.js

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
 else {
 
 // connection credentials
-const connection = mysql.createConnection({
+connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "Melvin Hernandez",
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,3 +6,17 @@ const router = express.Router();
 
 // import the burger.js 
 const burger = require("../models/burger.js")
+
+// create the routes and the logic within these routes
+// in reference to catsController.js in week-14 activity-17
+
+// default or home route
+router.get("/", (req, res)=>{
+    burger.selectAll( (data)=>{
+        const burgersObject = {
+            burgersCollected: data
+        };
+        console.log(burgersObject);
+        res.render("index", burgersObject);
+    });
+});
