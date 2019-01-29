1890e446f86241198764b8efa1bf1fbbd8ef389e
Melvin Hernandez <melher39@gmail.com>
2019-01-29 16:23:26 -0600

    formatted code and added/removed comments in the following files: burger.js, style.css, app.js, index and main.handlebars. project is pretty much done as far as functionality. need to style more and edit the readme file. but will test on heroku first before continuing

--- models/burger.js
+++ models/burger.js
@@ -1,21 +1,25 @@
 // import the ORM object
+// code inspired by week-14 activity-17 models->cat.js
 const orm = require("../config/orm.js");
 
 // code that will call the ORM methods
 const burger = {
     selectAll: (cb) => {
+        // burgers is the name of the table
         orm.selectAll("burgers", (res) => {
             cb(res);
         });
     },
 
     insertOne: (burgerName, cb) => {
+        // FALSE is automatically added when adding a burger because it will not be devoured
         orm.insertOne("burgers", burgerName, "FALSE", (res)=>{
             cb(res);
         });
     },
 
     updateOne: (condition, cb) => {
+        // condition is how the burger will be identified
         orm.updateOne("burgers", condition, (res) => {
             cb(res);
         });
@@ -23,4 +27,5 @@ const burger = {
     
 };
 
+// export these methods for the controller to use (burgers_controller.js)
 module.exports = burger;
